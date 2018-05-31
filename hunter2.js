7635119ac4e87b74cb55fd806be3bc8293386112
vivek1996 <mvivek3112@gmail.com>
2018-05-31 19:51:44 +0530

    hunter2 finished

---
+++ hunter2.js
@@ -0,0 +1,18 @@
+var removeDuplicate = () => {
+  let test = [];
+  let length = prompt("Enter the length");
+  length = Number(length);
+  for (let i = 1; i <= length; i++) {
+    let temp = prompt("enter the numbers");
+    test.push(temp);
+  }
+  let arr = test.filter((elem, pos, arr) => {
+    return arr.indexOf(elem) == pos;
+  });
+  arr.sort();
+  arr.reverse();
+  let result = arr.join("");
+  console.log(result);
+};
+
+removeDuplicate();
